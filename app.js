a7f77266b0492e4ffcc756e2005fd1a6e51dbb83
tzero86 <eliasmedina86@gmail.com>
2017-11-29 12:40:56 -0300

    Exercise Solved.
    Not much to view, neither was much to do here. Just a small and really simple JS challenge in a few lines of code.

---
+++ app.js
@@ -0,0 +1,6 @@
+let userName = prompt("Please enter your First Name.");
+let lastName = prompt("Please enter your last Name.");
+let userAge = prompt("Please enter your Age.");
+
+console.log("Your Full name is " + userName + " " + lastName);
+console.log("Your Age is " + userAge + " years old. ");
